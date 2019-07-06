8aa317f3d5a29e0be9e592b53abaf7714b66727e
Théo Naciri <theo.naciri@gmail.com>
2019-07-06 20:48:17 +0200

    first pivottable

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-/*
-import KingTable from "kingtable";
-import KingTableUtils from "kingtable/utils";
-*/
+// import Pivot from "pivottable";
 
-var table = new KingTable({
-  element:  document.getElementById("main-table"),
-  url: "/data/dump.json",
-  sortBy: "id",
-  columns: {
-  	options: {
-  		secret: 1
-  	}
-  }
-});
 
-table.render();
-table.on("fetch:done", function(e) {
-	console.warn('yo man', e);
-});
-console.log('table', table);
