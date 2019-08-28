4a2c542bef53fd4d9f7c068eb87c7e66e545dc4f
Théo Naciri <theo.naciri@gmail.com>
2019-08-28 15:10:40 +0200

    sails init

---
+++ api/controllers/log/view-logs.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+
+  friendlyName: 'View privacy',
+
+
+  description: 'Display "Privacy policy" page.',
+
+
+  exits: {
+
+    success: {
+      viewTemplatePath: 'pages/log/table'
+    }
+
+  },
+
+
+  fn: async function () {
+
+    // All done.
+    return {};
+
+  }
+
+
+};
