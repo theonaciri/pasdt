2ed0eabf322a997f78ee3f14b7e58fda68db6af5
Théo Naciri <theo.naciri@gmail.com>
2019-08-30 11:49:55 +0200

    remove kingtable, add datatables

--- assets/js/pages/dashboard/welcome.page.js
+++ assets/js/pages/dashboard/welcome.page.js
@@ -29,10 +29,10 @@ parasails.registerPage('welcome', {
     // corresponds with `:unused?` in the server-side route for this page.
     switch (virtualPageSlug) {
       case 'hello':
-        this.modal = 'example';
-        break;
+      this.modal = 'example';
+      break;
       default:
-        this.modal = '';
+      this.modal = '';
     }
   },
 
@@ -43,6 +43,63 @@ parasails.registerPage('welcome', {
 
     _initTable() {
       console.log('init');
+      $(document).ready( function () {
+        // Setup - add a text input to each footer cell
+        $('#main-table tfoot th').each( function () {
+          var title = $(this).text();
+          $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+        } );
+        var table = $('#main-table').DataTable({
+          language: {
+            processing:     "Traitement en cours...",
+            search:         "Rechercher&nbsp;:",
+            lengthMenu:     "Afficher _MENU_ &eacute;l&eacute;ments",
+            info:           "Affichage de l'&eacute;lement _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
+            infoEmpty:      "Affichage de l'&eacute;lement 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
+            infoFiltered:   "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
+            infoPostFix:    "",
+            loadingRecords: "Chargement en cours...",
+            zeroRecords:    "Aucun &eacute;l&eacute;ment &agrave; afficher",
+            emptyTable:     "Aucune donnée disponible dans le tableau",
+            paginate: {
+              first:      "Premier",
+              previous:   "Pr&eacute;c&eacute;dent",
+              next:       "Suivant",
+              last:       "Dernier"
+            },
+            aria: {
+              sortAscending:  ": activer pour trier la colonne par ordre croissant",
+              sortDescending: ": activer pour trier la colonne par ordre décroissant"
+            }
+          },
+          "ajax": {
+            "url": "/data/dump.json",
+            "dataSrc": ""
+          },
+          "columns": [
+          {"data": "id"},
+          {"data": "cardId"},
+          {"data": "msgId"},
+          {"data": "msg"},
+          {"data": "eventType"},
+              //{"data": "options"},
+              {"data": "created_at"},
+              {"data": "updated_at"},
+              ]
+            });
+      table.columns().every( function () {
+        var that = this;
+        
+        $( 'input', this.footer() ).on( 'keyup change clear', function () {
+          if ( that.search() !== this.value ) {
+            that
+            .search( this.value )
+            .draw();
+          }
+        } );
+      } );
+      } );
+      /*
       var table = new KingTable({
         element:  document.getElementById("main-table"),
         url: "/data/dump.json",
@@ -56,9 +113,9 @@ parasails.registerPage('welcome', {
 
       table.render();
       table.on("fetch:done", function(e) {
-        console.warn('yo man', e);
       });
       console.log('table', table);
+      */
     },
     clickOpenExampleModalButton: async function() {
       this.goto('/welcome/hello');
