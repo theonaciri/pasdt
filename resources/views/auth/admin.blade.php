a88fcdf00f5afcaf869e56569f26cf148ab5e064
Théo Naciri <theo.naciri@gmail.com>
2019-10-23 14:35:42 +0200

    translation

--- resources/views/auth/admin.blade.php
+++ resources/views/auth/admin.blade.php
@@ -68,11 +68,11 @@
                             <button type="button" class="close" data-dismiss="alert">×</button>
                             <strong>{{ $message }}</strong>
                         </div>
-                        <img src="images/companylogos/{{ Session::get('image') }}">
+                        <!-- <img src="images/companylogos/{{ Session::get('image') }}"> -->
                         @endif
                         @if (count($errors) > 0)
                         <div class="alert alert-danger">
-                            <strong>Whoops!</strong> There were some problems with your input.
+                            <strong>Whoops!</strong> Il y a eu un problème avec votre image.
                             <ul>
                                 @foreach ($errors->all() as $error)
                                 <li>{{ $error }}</li>
