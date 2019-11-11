cb1e82ebb848dddb521600de9481d5c43351f44e
Theo Naciri <theo.naciri@gmail.com>
2019-11-11 12:29:48 +0100

    visu logs

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -14,9 +14,36 @@ class LogController extends Controller
      *
      * @return void
      */
-    public function __construct(\Illuminate\Http\Request $request)
+    public function __construct(Request $request)
     {
        // $this->middleware('auth');
+    }
+
+    /**
+    * Get data
+    * Connexion web
+    */
+
+    public function getAllData(Request $request) {
+        $this->middleware('auth');
+        return response()->json(Log::all());
+    }
+
+    /**
+     * Store single log
+     * Connexion API
+     */
+    public function storeData(Request $request)
+    {
+        $this->authAPI($request);
+        $log = $request->json()->all();
+        $newlog = new Log();
+        $newlog->fill($log);
+        $newlog->save();
+        return response()->json('{"ok": "ok"}');
+    }
+
+    protected function authAPI(Request $request) {
         $token = $request->header('Api-Key');
         if (env('API_TOKEN', false) !== $token) {
             abort(403, 'Action non authorisée.');
@@ -25,35 +52,5 @@ class LogController extends Controller
         if (empty($this->user)) {
             abort(403, "Echec de l'authentification.");
         }
-    }
-
-    /**
-     * Store logs.
-     *
-     */
-    public function storeData(Request $request)
-    {
-        $log = $request->json()->all();
-        $newlog = new Log();
-        $newlog->fill($log);
-        $newlog->save();
-        return response()->json('{"ok": "ok"}');
-    }
-
-    public function salut() {
-        return 'OKKK';
-    }
-
-    /**
-     * Get the bearer token from the request headers.
-     *
-     * @return string|null
-    */
-    public function bearerToken()
-    {
-       $header = $this->header('Api-Key', '');
-       if (Str::startsWith($header, 'Bearer ')) {
-                return Str::substr($header, 7);
-       }
     }
 }
