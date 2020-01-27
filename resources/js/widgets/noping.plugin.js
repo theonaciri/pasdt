d6157808200c95ce71bf6ba9b16ba784756fbb92
Théo Naciri <theo.naciri@gmail.com>
2020-01-27 17:46:17 +0100

    module buttons

--- resources/js/widgets/noping.plugin.js
+++ resources/js/widgets/noping.plugin.js
@@ -1,4 +1,5 @@
-function initNopingButtons(table) {
+define(['jquery'], function($) {
+	function initNopingButtons(table) {
 	//timeout for whatever reason datatables is not initialised
 		$("#noday").click(function(e) {
 			ext_search(table, $(e.currentTarget), true);
@@ -10,28 +11,29 @@ function initNopingButtons(table) {
 		setTimeout(function(){
 			ext_search(table, $("#noday"), false);
 		}, 100);
-}
+	}
 
-function ext_search(table, $target, toggle) {
-	var togglevalue = localStorage.getItem('noping') == "true";
-	togglevalue = toggle ? !togglevalue : !!togglevalue;
-	console.log('toogle', togglevalue);
-	localStorage.setItem('noping', togglevalue);
-	$target.toggleClass('btn-dark', togglevalue);
-	$.fn.dataTable.ext.search.push(
-        function( settings, data, dataIndex ) {
-        	if (togglevalue) {
-            	return data[3] != 'Day' && data[3] != 'Ack' && data[3] != 'Test';
-        	} else {
-        		return true;
-        	}
-        }
-    );
-    table.draw();
-    $('#main-table').find('.th-message').html(togglevalue ? 'Anomalies' : 'Message');
-    $('#main-table').find('.th-input-message input').attr('placeholder', togglevalue ? 'Rechercher Anomalie' : 'Rechercher Message');
-    $.fn.dataTable.ext.search.pop();
-}
-
-
-window.initNopingButtons = initNopingButtons;
+	function ext_search(table, $target, toggle) {
+		var togglevalue = localStorage.getItem('noping') == "true";
+		togglevalue = toggle ? !togglevalue : !!togglevalue;
+		localStorage.setItem('noping', togglevalue);
+		$target.toggleClass('btn-dark', togglevalue);
+		$.fn.dataTable.ext.search.push(
+	        function( settings, data, dataIndex ) {
+	        	console.log("data", data, data[3]);
+	        	if (togglevalue) {
+	        		console.warn(data[3] != 'Day' && data[3] != 'Ack' && data[3] != 'Test')
+	            	return data[3] != 'Day' && data[3] != 'Ack' && data[3] != 'Test';
+	        	} else {
+	        		console.error('false');
+	        		return true;
+	        	}
+	        }
+	    );
+	    table.draw();
+	    $('#main-table').find('.th-message').html(togglevalue ? 'Anomalies' : 'Message');
+	    $('#main-table').find('.th-input-message input').attr('placeholder', togglevalue ? 'Rechercher Anomalie' : 'Rechercher Message');
+	    $.fn.dataTable.ext.search.pop();
+	}
+	return {initNopingButtons: initNopingButtons};
+});
