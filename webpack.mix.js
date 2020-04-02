4965d3bec988a6caad077578dd5d1317853ec69d
Theo Naciri <theo.naciri@gmail.com>
2020-04-02 09:17:13 +0200

    color popup

--- webpack.mix.js
+++ webpack.mix.js
@@ -38,7 +38,7 @@ mix.webpackConfig({
 });
 
 mix.js('resources/js/app.js', 'public/js')
-   //.babel('public/js/app.js', 'public/js/app.es5.js')
+   .babel('public/js/app.js', 'public/js/app.es5.js')
    //.js('resources/js/checkout.js', 'public/js')
    //.babel('public/js/checkout.js', 'public/js/checkout.es5.js')
    .sass('resources/sass/app.scss', 'public/css')
