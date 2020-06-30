efb2c5b68332f5c168d6feb7c7b9f0632f5cf030
Theo Naciri <theo.naciri@gmail.com>
2020-06-30 09:38:18 +0200

    corrected temp decrease detection

--- config/pasdt.php
+++ config/pasdt.php
@@ -9,7 +9,7 @@ return [
 		"TEMP_CRIT_LOW" => 0,
 		"TEMP_HIGH" => 60,
 		"TEMP_CRIT_HIGH" => 75,
-		"TEMP_DECREASE" => 20,
+		"TEMP_DECREASE" => -20,
 		"TEMP_INCREASE" => 20,
 		"NO_LOG" => 7200 /* 2h */
 		/*"TOO_MANY_LOGS" => 60,*/
