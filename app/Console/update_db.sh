889ef54c7c7fa83a317fe47e168b1e3d00be7374
Theo Naciri <theo.naciri@gmail.com>
2020-07-02 04:25:54 +0200

    array_filter for PHP < 7.4

--- app/Console/update_db.sh
+++ app/Console/update_db.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-mysql -u pasdt -pZiAEfboYP0uv -h pasdt003.spin-io.fr -P 3306 -D pasdt -e "select id, cardId, msg, eventType, options, created_at, updated_at from log ORDER BY id DESC" > public/json/data/dump.csv && csvtojson --delimiter="\t" public/json/data/dump.csv > public/json/data/dump.json
+cd /homez.112/cfmhtacoep/www/pasdt && php artisan schedule:run >> /dev/null 2>&1
