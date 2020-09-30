fe3f7915980b20fd3945af04d46c262cc97e241a
Theo Naciri <theo.naciri@gmail.com>
2020-09-30 09:13:36 +0200

    error 419

---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,6 @@
+@extends('errors::minimal')
+
+@section('title', __('Page Expired'))
+@section('code', '419')
+<p>Fermez tous vos onglets PASDT afin que l'application puisse se mettre à jour.</p>
+@section('message', __('Page Expired'))
