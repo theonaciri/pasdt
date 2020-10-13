dd811596f22eeabea73efb037161132bc42c791b
Théo Naciri <theo.naciri@gmail.com>
2020-10-13 12:15:43 +0200

    sw v4

--- public/service-worker.js
+++ public/service-worker.js
@@ -16,7 +16,7 @@ const filesToCache = [
   'images/logo-192.png'
 ];
 
-const staticCacheName = 'pages-cache-v3';
+const staticCacheName = 'pages-cache-v4';
 
 function stripQueryStringAndHashFromPath(url) {
   return url.split("?")[0].split("#")[0];
@@ -26,9 +26,9 @@ self.addEventListener('install', event => {
   /*console.log('Attempting to install service worker and cache static assets');*/
   event.waitUntil(
     caches.open(staticCacheName)
-    .then(cache => {
-      return cache.addAll(filesToCache);
-    })
+      .then(cache => {
+        return cache.addAll(filesToCache);
+      })
   );
 });
 
@@ -69,36 +69,36 @@ function cleanResponse(response) {
 
 self.addEventListener('fetch', event => {
   /*console.log('Fetch event for ', event.request.url);*/
-  if (event.request.url.indexOf("csrf") !== -1) { return ;}
+  if (event.request.url.indexOf("csrf") !== -1) { return; }
   /*if (event.request.url.indexOf('fonts/') !== -1) {
     event.request = new Request(stripQueryStringAndHashFromPath(event.request.url), event.request);
     
   }*/
   event.respondWith(
     caches.match(event.request)
-    .then(response => {
-      if (response) {
-        if (response.redirected) {
-          console.warn('redirected!', response);
-          response = cleanResponse(response);
-          console.warn('new response', response);
-        }
-        /*console.log('Found ', event.request.url, ' in cache');*/
-        return response;
-      }
-      /*console.log('Network request for ', event.request.url);*/
-      return fetch(event.request)
       .then(response => {
-        /*if (response.status === 404) {
-          return caches.match('pages/404.html');
-        }*/
-        return caches.open(staticCacheName)
-        .then(cache => {
-          cache.put(event.request.url, response.clone());
+        if (response) {
+          if (response.redirected) {
+            console.warn('redirected!', response);
+            response = cleanResponse(response);
+            console.warn('new response', response);
+          }
+          /*console.log('Found ', event.request.url, ' in cache');*/
           return response;
-        });
-      });
-    })/*.catch(error => {
+        }
+        /*console.log('Network request for ', event.request.url);*/
+        return fetch(event.request)
+          .then(response => {
+            /*if (response.status === 404) {
+              return caches.match('pages/404.html');
+            }*/
+            return caches.open(staticCacheName)
+              .then(cache => {
+                cache.put(event.request.url, response.clone());
+                return response;
+              });
+          });
+      })/*.catch(error => {
       console.log('Error, ', error);
       return caches.match('pages/offline.html');
     })*/
