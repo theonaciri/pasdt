0194ecae08d261b56dc0bd760c20121d9da2e789
Theo Naciri <theo.naciri@gmail.com>
2020-10-14 16:24:01 +0200

    no sw in login

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -106,18 +106,6 @@
         </div>
 
     </div>
-    <script>
-        // Register service worker
-        if ('serviceWorker' in navigator) {
-            window.addEventListener('load', () => {
-            navigator.serviceWorker.register('/service-worker.js')
-                .then((reg) => {
-                    /*console.log('Service worker registered.', reg);*/
-                });
-            });
-        }
-
-    </script>
 </body>
 
 </html>
