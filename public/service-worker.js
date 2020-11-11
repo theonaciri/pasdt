696760efbfcf1098e50a7fd8f40d5443e734921d
Theo Naciri <theo.naciri@gmail.com>
2020-11-11 18:03:33 +0100

    sw

--- public/service-worker.js
+++ public/service-worker.js
@@ -19,7 +19,7 @@ const filesToCache = [
 var blacklist = ['/csrf', '/logs/', '/notifs', '/su_admin', '/client'];
 var whitelist = ['fonts/open-iconic', 'images/'];
 
-const staticCacheName = 'pages-cache-v18++';
+const staticCacheName = 'pages-cache-v19';
 
 function stripQueryStringAndHashFromPath(url) {
   return url.split("?")[0].split("#")[0];
