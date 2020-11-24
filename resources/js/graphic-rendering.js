6c7c2f63acc1df5e4435935bd3b23b042350dbc5
scaraude <ludodante@hotmail.fr>
2020-11-24 16:14:22 +0100

    Séparation des calcules pour la courbe régressive dans un fichier

--- resources/js/graphic-rendering.js
+++ resources/js/graphic-rendering.js
@@ -1,5 +1,5 @@
-define(["jquery", 'moment', "./components/lang", "./dependencies/lagrange" /*, "anychart", "anychart-jquery"*/],
-	function ($, moment, lang) {
+define(["jquery", 'moment', "./components/lang", "./dependencies/curve" /*, "anychart", "anychart-jquery"*/],
+	function ($, moment, lang, curve) {
 		window.chart = null;
 		var $mod_select = $('#graphModuleSelect');
 		var data = null;
@@ -7,7 +7,7 @@ define(["jquery", 'moment', "./components/lang", "./dependencies/lagrange" /*, "
 		var active_module = localStorage.getItem('graph-active-module');
 		var interval_var = null;
 		const fromDate = new Date("2018-04-25 13:40:24");
-
+debugger
 		function init() {
 			if (chart != null) return; // only one init;
 
@@ -71,111 +71,105 @@ define(["jquery", 'moment', "./components/lang", "./dependencies/lagrange" /*, "
 		// 	return dataWithAverage;
 		// };
 
-		function getRegressionCurve(data) {
-			const projectedOnDays = 3;
-			const coeff = regLin(data.map(({ maxtemp }) => maxtemp));
-			const timeCoeff = getUnitOfTime(data, projectedOnDays);
+		// function getRegressiveCurve(data) {
+		// 	const projectedOnDays = 3;
+		// 	const coeff = regLin(data.map(({ maxtemp }) => maxtemp));
+		// 	const timeCoeff = getUnitOfTime(data, projectedOnDays);
 
-			let tabResult = [];
+		// 	let tabResult = [];
 
-			for (let index = 0; index < data.length; index++) {
-				tabResult.push({
-					created_at: data[index].created_at,
-					maxtemp: data[index].maxtemp,
-					average: (Math.round((coeff.a * index + coeff.b) * 100) / 100)
-				})
-			}
+		// 	for (let index = 0; index < data.length; index++) {
+		// 		tabResult.push({
+		// 			created_at: data[index].created_at,
+		// 			maxtemp: data[index].maxtemp,
+		// 			average: (Math.round((coeff.a * index + coeff.b) * 100) / 100)
+		// 		})
+		// 	}
 
+		// 	let estimated_data = [];
+		// 	for (let i = 0; i < timeCoeff.nb; i++) {
 
-			let estimated_data = [];
-			// [{
-			// 	projectedAverage: tabResult[data.length - 1].average,
-			// 	created_at: data[data.length - 1].created_at
-			// }];
-			for (let i = 0; i < timeCoeff.nb; i++) {
+		// 		const tempMax = 250;
+		// 		const tempMin = -50;
+		// 		let calculatedTemps = (Math.round((coeff.a * (data.length + i) + coeff.b) * 100) / 100) > tempMax ?
+		// 			tempMax : ((Math.round((coeff.a * (data.length + i) + coeff.b) * 100) / 100) < tempMin) ?
+		// 				tempMin : (Math.round((coeff.a * (data.length + i) + coeff.b) * 100) / 100);
 
+		// 		let dateTime = new Date();
+		// 		const last_date = new Date(data[data.length - 1].created_at);
+		// 		dateTime.setTime(last_date.getTime() + (i + 1) * timeCoeff.ut)
+		// 		let transformedDate = moment(dateTime).format("YYYY-MM-DD HH:mm:ss");
 
-				const tempMax = 250;
-				const tempMin = -50;
-				let calculatedTemps = (Math.round((coeff.a * (data.length + i) + coeff.b) * 100) / 100) > tempMax ?
-					tempMax : ((Math.round((coeff.a * (data.length + i) + coeff.b) * 100) / 100) < tempMin) ?
-						tempMin : (Math.round((coeff.a * (data.length + i) + coeff.b) * 100) / 100);
+		// 		estimated_data.push({
+		// 			//average: calculatedTemps,
+		// 			average: calculatedTemps,
+		// 			created_at: transformedDate
+		// 		})
+		// 	}
 
-				let dateTime = new Date();
-				const last_date = new Date(data[data.length - 1].created_at);
-				dateTime.setTime(last_date.getTime() + (i + 1) * timeCoeff.ut)
-				let transformedDate = moment(dateTime).format("YYYY-MM-DD HH:mm:ss");
+		// 	tabResult = tabResult.concat(estimated_data);
+		// 	return tabResult;
+		// }
 
-				estimated_data.push({
-					//average: calculatedTemps,
-					average: calculatedTemps,
-					created_at: transformedDate
-				})
-			}
+		// function getUnitOfTime(data, days) {
+		// 	let first_date = new Date(data[0].created_at);
+		// 	let last_date = new Date(data[data.length - 1].created_at);
+		// 	// let start_date, end_date = new Date();
 
-			tabResult = tabResult.concat(estimated_data);
-			return tabResult;
-		}
+		// 	let unit_of_time = 0;
 
-		function getUnitOfTime(data, days) {
-			let first_date = new Date(data[0].created_at);
-			let last_date = new Date(data[data.length - 1].created_at);
-			// let start_date, end_date = new Date();
+		// 	if (first_date > last_date) {//(first_date.getTime() > last_date.getTime()) {
+		// 		unit_of_time = Math.round((first_date.getTime() - last_date.getTime()) / data.length);
+		// 		// start_date = last_date;
+		// 		// end_date = first_date;
+		// 	} else {
+		// 		unit_of_time = Math.round((last_date.getTime() - first_date.getTime()) / data.length);
+		// 		// start_date = first_date;
+		// 		// end_date = last_date;
+		// 	}
 
-			let unit_of_time = 0;
+		// 	//milliseconds * second * minutes * hours * days (projected on "x" days)
+		// 	let nb_val_calculated = (Math.round(1000 * 60 * 60 * 24 * days / unit_of_time)) < 500 ?
+		// 		(Math.round(1000 * 60 * 60 * 24 * days / unit_of_time)) : 500;
 
-			if (first_date > last_date) {//(first_date.getTime() > last_date.getTime()) {
-				unit_of_time = Math.round((first_date.getTime() - last_date.getTime()) / data.length);
-				// start_date = last_date;
-				// end_date = first_date;
-			} else {
-				unit_of_time = Math.round((last_date.getTime() - first_date.getTime()) / data.length);
-				// start_date = first_date;
-				// end_date = last_date;
-			}
+		// 	return { nb: nb_val_calculated, ut: unit_of_time }; //end_date;
+		// }
 
-			//milliseconds * second * minutes * hours * days (projected on "x" days)
-			let nb_val_calculated = (Math.round(1000 * 60 * 60 * 24 * days / unit_of_time)) < 500 ?
-				(Math.round(1000 * 60 * 60 * 24 * days / unit_of_time)) : 500;
+		// function regLin(points_y) {
+		// 	// Ajuste une droite d'équation a*x + b sur les points (x, y) par la méthode
+		// 	// des moindres carrés.
 
-			return { nb: nb_val_calculated, ut: unit_of_time }; //end_date;
-		}
+		// 	// 		Args :
+		// 	// * x(list): valeurs de x
+		// 	// 		* y(list): valeurs de y
+		// 	// 	Return:
+		// 	// * a(float): pente de la droite
+		// 	// 		* b(float): ordonnée à l'origine
 
-		function regLin(points_y) {
-			// Ajuste une droite d'équation a*x + b sur les points (x, y) par la méthode
-			// des moindres carrés.
-
-			// 		Args :
-			// * x(list): valeurs de x
-			// 		* y(list): valeurs de y
-			// 	Return:
-			// * a(float): pente de la droite
-			// 		* b(float): ordonnée à l'origine
-
-			// # initialisation des sommes
-			let x_sum = 0;
-			let x2_sum = 0;
-			let y_sum = 0;
-			let xy_sum = 0;
-			try {
-				// # calcul des sommes
-				for (let index = 0; index < points_y.length; index++) {
-					x_sum += index;
-					x2_sum += index ** 2;
-					y_sum += points_y[index];
-					xy_sum += index * points_y[index];
-				}
-				// # nombre de points
-				npoints = points_y.length;
-				// # calcul des paramétras
-				const a = (npoints * xy_sum - x_sum * y_sum) / (npoints * x2_sum - x_sum ** 2);
-				const b = (x2_sum * y_sum - x_sum * xy_sum) / (npoints * x2_sum - x_sum ** 2);
-				// # renvoie des parametres
-				return { a: a, b: b };
-			} catch (error) {
-				console.log(error);
-			}
-		}
+		// 	// # initialisation des sommes
+		// 	let x_sum = 0;
+		// 	let x2_sum = 0;
+		// 	let y_sum = 0;
+		// 	let xy_sum = 0;
+		// 	try {
+		// 		// # calcul des sommes
+		// 		for (let index = 0; index < points_y.length; index++) {
+		// 			x_sum += index;
+		// 			x2_sum += index ** 2;
+		// 			y_sum += points_y[index];
+		// 			xy_sum += index * points_y[index];
+		// 		}
+		// 		// # nombre de points
+		// 		npoints = points_y.length;
+		// 		// # calcul des paramétras
+		// 		const a = (npoints * xy_sum - x_sum * y_sum) / (npoints * x2_sum - x_sum ** 2);
+		// 		const b = (x2_sum * y_sum - x_sum * xy_sum) / (npoints * x2_sum - x_sum ** 2);
+		// 		// # renvoie des parametres
+		// 		return { a: a, b: b };
+		// 	} catch (error) {
+		// 		console.log(error);
+		// 	}
+		// }
 
 		function setModuleSelect() {
 			if ($mod_select.val() !== null) return; // already initialized
@@ -221,9 +215,8 @@ define(["jquery", 'moment', "./components/lang", "./dependencies/lagrange" /*, "
 			// create a table
 			var dataTable = anychart.data.table('created_at');
 
-			const tempsWithAverage = getRegressionCurve(data.temps);
-			// const projectedDatas = getProjectedCurve(tempsWithAverage);
-			// const tempsWithAll = tempsWithAverage.concat(projectedDatas);
+			// get regressive curve
+			const tempsWithAverage = curve(data.temps);
 			// add data
 			dataTable.addData(tempsWithAverage);
 
@@ -250,13 +243,6 @@ define(["jquery", 'moment', "./components/lang", "./dependencies/lagrange" /*, "
 						return this.seriesName + ": " + this.value + "°C";
 				});
 			//plot.xAxis().labels().format(function() {return new Date(this.value).toLocaleDateString("fr-FR", date_options)});
-
-			// var projectedAverage = plot.spline(
-			// 	dataTable.mapAs({
-			// 		value: "projectedAverage",
-			// 		x: "created_at"
-			// 	})
-			// ).name("Provisional").stroke({ color: 'cyan', dash: "5 5" });
 
 			var average = plot.spline(
 				dataTable.mapAs({
