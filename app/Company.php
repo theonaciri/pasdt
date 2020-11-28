ea09af0e96b696b8f480e8e9203a4e2f948fdd9d
scaraude <ludodante@hotmail.fr>
2020-11-28 14:32:40 +0100

    front done, back in progress

--- app/Company.php
+++ app/Company.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'logo', 'colors', 'admin_id'
+    ];
+
     /*public function admins()
     {
     	return $this->hasMany(Product::class);
