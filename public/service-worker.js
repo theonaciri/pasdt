2c71e2f9708066f3496105a02174409aa5c1b557
Theo Naciri <theo.naciri@gmail.com>
2020-11-29 00:49:13 +0100

    locale js singleton

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,7 +18,7 @@ const filesToCache = [
 var blacklist = ['/csrf', '/logs/', '/notifs', '/su_admin', '/client', '/register'];
 var whitelist = ['fonts/open-iconic', 'images/', 'json/locales/'];
 
-const staticCacheName = 'pages-cache-v24.0';
+const staticCacheName = 'pages-cache-v24.32';
 
 function stripQueryStringAndHashFromPath(url) {
   return url.split("?")[0].split("#")[0];
--- resources/js/client.js
+++ resources/js/client.js
@@ -134,6 +134,7 @@ define(['jquery', 'moment/moment', './components/getURLParameter',
 	if (typeof locale != "undefined" && locale != "en-us" && typeof moment_locale !== "undefined") {
 		moment.updateLocale(locale.split("-")[0], moment_locale);
 	}
+	
 	var $logs = $('#notifTable > tbody > tr');
 	$logs.each(function() {
 		var format = (locale === "en-us" ? "MM/DD/YY" : "DD/MM/YY") + " [" + lang("at") + "] HH:mm";
@@ -153,7 +154,7 @@ define(['jquery', 'moment/moment', './components/getURLParameter',
 		$resolved.html((success ? lang("During") + " " : lang("Since") + " ")
 			+ moment.duration(nowdate.diff(created)).humanize())
 			.prop('title', success ? lang("Solved the : ") + nowdate.format(format) : lang("Still ongoing"))
-			.tooltip('_fixTitle');
+			.toggleNotificationpermission('_fixTitle');
 		$created.html(created.calendar({sameElse: "[" + lang("The") + "] " + format}).capitalize())
 				.prop('title', lang("First occurence the : ") + created.format(format))
 				.tooltip('_fixTitle');
