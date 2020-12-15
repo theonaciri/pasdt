16ef6936f2bf552d8a700b2648ad9f5a8b0e5550
Theo Naciri <theo.naciri@gmail.com>
2020-12-15 16:30:33 +0100

    err type translatoin

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -205,12 +205,16 @@ EOTNOTIF;
         if ($id_company === -1) {
             $id_company = "%%";
         }
-        return Notification::select('notifications.id', 'comment', 'modules.name AS name', 'modules.module_id AS module_id', 'modules.id AS id_module', 'send_mails', 'type', 'log', 'value', 'occurences', 'resolved', 'notifications.created_at', 'notifications.resolved_at')
+        $notifs = Notification::select('notifications.id', 'comment', 'modules.name AS name', 'modules.module_id AS module_id', 'modules.id AS id_module', 'send_mails', 'type', 'log', 'value', 'occurences', 'resolved', 'notifications.created_at', 'notifications.resolved_at')
             ->where('seen', $seen)
             ->leftJoin('modules', 'modules.module_id', '=', 'notifications.module')
             ->where('modules.company_id', "LIKE", $id_company)
             ->orderBy('notifications.resolved_at', 'DESC')
             ->limit($limit)->get();
+        for ($i=0; $i < count($notifs); $i++) { 
+            $notifs[$i]->type_text = __($notifs[$i]->type);
+        }
+        return $notifs;
     }
 
     public static function getNotifsCount(Request $request, int $seen = 0) {
--- resources/js/init.js
+++ resources/js/init.js
@@ -1,45 +1,47 @@
 define(['jquery', 'js-cookie', './graphic-rendering', 'moment', /*'./dependencies/moment-fr.js',*/
 	'./components/cache', './components/notifs', './bootstrap', 'bootstrap'],
-	function($, cookie, graph_annotated, moment) {
-	
-	$(function() {
-		$('[data-toggle="tooltip"]').tooltip({ trigger: "hover" });
-		$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-			// e.target // newly activated tab
-			// e.relatedTarget // previous active tab
-			$('table.dataTable').DataTable().fixedHeader.adjust();
-			localStorage.setItem('opened-tab', e.target.getAttribute('id'));
-		});
-	    $('#graphs-live-tab').on("click", function () {
-			$.ajaxSetup({cache: true});
-
-	  		$.getScript("/js/anychart-bundle.js")
-	  		.done(function() {
-	     		graph_annotated.init();
-	  		});
-			
-			var cssLink = $("<link>");
-			$("head").append(cssLink); //IE hack: append before setting href
-			cssLink.attr({
-			  	rel:  "stylesheet",
-			  	type: "text/css",
-			  	href: "/css/anychart-ui.min.css"
-			});
-	  		$.ajaxSetup({cache: false});
-	    });
-
-		$('#logoff-button, #reconnect-button').on("click", function(event) {
-			event.preventDefault();
-	       	if(window.navigator && navigator.serviceWorker) {
-	          	navigator.serviceWorker.getRegistrations()
-	          	.then(function(registrations) {
-		            for(let registration of registrations) {
-		              registration.unregister();
-		            }
-	          	});
-	        }
-	        document.getElementById('logout-form').submit();
-		});
+function($, cookie, graph_annotated, moment) {
+	$('[data-toggle="tooltip"]').tooltip({ trigger: "hover" });
+	$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+		// e.target // newly activated tab
+		// e.relatedTarget // previous active tab
+		$('table.dataTable').DataTable().fixedHeader.adjust();
+		localStorage.setItem('opened-tab', e.target.getAttribute('id'));
 	});
+    $('#graphs-live-tab').on("click", function () {
+		$.ajaxSetup({cache: true});
 
-})
+  		$.getScript("/js/anychart-bundle.js")
+  		.done(function() {
+     		graph_annotated.init();
+  		});
+		
+		var cssLink = $("<link>");
+		$("head").append(cssLink); //IE hack: append before setting href
+		cssLink.attr({
+		  	rel:  "stylesheet",
+		  	type: "text/css",
+		  	href: "/css/anychart-ui.min.css"
+		});
+  		$.ajaxSetup({cache: false});
+    });
+
+	$('#logoff-button, #reconnect-button').on("click", function(event) {
+		event.preventDefault();
+       	if(window.navigator && navigator.serviceWorker) {
+          	navigator.serviceWorker.getRegistrations()
+          	.then(function(registrations) {
+	            for(let registration of registrations) {
+	              registration.unregister();
+	            }
+          	});
+        }
+        document.getElementById('logout-form').submit();
+	});
+	$(function() {
+		if (location.pathname === "/consultation" || location.pathname === "/") {
+			var opened_tab = localStorage.getItem('opened-tab') || 'synthesis-tab';
+			$('#' + opened_tab).click();
+		}	
+	})
+});
